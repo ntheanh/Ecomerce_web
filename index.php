3d3c35e5b94a10d5066869f403947927de658164
Ty <Tyu@example.com>
2023-11-16 20:26:46 +0700

    header

--- index.php
+++ index.php
@@ -1,14 +1,89 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Phụ kiện điện thoại</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@400;500;600;700;800;900&display=swap"
+        rel="stylesheet">
+    <title>Plant Shop</title>
+
 </head>
+
 <body>
     <div class="wrapper">
-        
+        <section class="header_wrapper">
+            <div class="header">
+                <div class="navbar">
+                    <div class="navbar__logo"><a href="/">GROW</a></div>
+                    <ul class="navbar__list">
+                        <li class="navbar__list--item">
+                            <a href="/">Home</a>
+                        </li>
+                        <li class="navbar__list--item">
+                            <a href="/shop">Shop</a>
+                            <ul class="navbar__list--item-sub">
+                                <li class="navbar__item--sub"><a href="">Cactus</a></li>
+                                <li class="navbar__item--sub"><a href="">Succulent</a></li>
+                                <li class="navbar__item--sub"><a href="">Indoor Plants</a></li>
+                                <li class="navbar__item--sub"><a href="">Bonsai</a></li>
+                            </ul>
+                        </li>
+                        <li class="navbar__list--item">
+                            <a href="/">About US</a>
+                        </li>
+                        <li class="navbar__list--item">
+                            <a href="/">Testimonial</a>
+                        </li>
+                        <li class="navbar__list--item">
+                            <a href="/giohang">
+                                <img src="icon/cart.png" alt="car" class="navbar__cart--icon">
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="slogan">
+                <h4>The Boutique Store For Plants!</h4>
+                <span>
+                    Everything Is Better<br />With Plants
+                </span>
+                <button class="btn-shop">Shop Now</button>
+            </div>
+
+        </section>
+
+        <div class="menu">
+            <p>Menu</p>
+
+        </div>
+        <div class="main">
+            <p>Main</p>
+            <p>Main</p>
+            <p>Main</p>
+            <p>Main</p>
+            <p>Main</p>
+            <p>Main</p>
+            <p>Main</p>
+            <p>Main</p>
+            <p>Main</p>
+            <p>Main</p>
+            <p>Main</p>
+            <p>Main</p>
+            <p>Main</p>
+            <p>Main</p>
+            <p>Main</p>
+            <p>Main</p>
+            <p>Main</p>
+            <p>Main</p>
+            <p>Main</p>
+        </div>
+        <div class="footer">
+            <p>Footer</p>
+        </div>
     </div>
 </body>
+
 </html>
